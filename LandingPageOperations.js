7786449c3170000a383cf6862c9d27b89f0d09bb
Azari <itsazari9321@gmail.com>
2022-04-27 18:27:19 -0400

    NewFixedVersion

---
+++ LandingPageOperations.js
@@ -0,0 +1,5 @@
+BroItsTheOnlyButton = document.getElementById('butt')
+
+
+
+BroItsTheOnlyButton.addEventListener('click', () => {alert(' Yo We Got The Website running and now we just gonna make it make it public and call it wrap  ')})
